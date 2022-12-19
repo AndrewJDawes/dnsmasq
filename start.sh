fd7945b78ef77ccf174a5c99b6479746df50f718
Andrew Dawes <andrewjamesdawes@gmail.com>
2022-12-18 21:54:54 -0500

    Fix some env variable bugs and reformat

--- start.sh
+++ start.sh
@@ -2,5 +2,13 @@
 if [ -f "getenv.sh" ]; then
     source getenv.sh
 fi
-eval_cmd="docker run -d --name $PROJECT_DOCKER_CONTAINER_NAME -p 53:53/udp -p 53:53/tcp -v "$(pwd)"/data/app/hosts.d:/app/hosts.d -v "$(pwd)"/data/etc/dnsmasq.d:/etc/dnsmasq.d $PROJECT_DOCKER_FULL_PATH"
+eval_cmd="docker run \
+-d \
+--name $PROJECT_DOCKER_CONTAINER_NAME \
+-p 53:53/udp \
+-p 53:53/tcp \
+-v "$(pwd)"/data/app/hosts.d:/app/hosts.d 
+-v "$(pwd)"/data/etc/dnsmasq.d:/etc/dnsmasq.d \
+$PROJECT_DOCKER_FULL_PATH
+"
 eval "$eval_cmd"
